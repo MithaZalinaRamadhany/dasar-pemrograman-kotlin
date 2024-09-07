c7813821577d74317927f3f9670b79735556ed2e
MithaZalinaRamadhany <mitazalina974@gmail.com>
2024-09-07 21:36:14 +0700

    Add Var.kt

---
+++ src/main/kotlin/Val.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var varVariabel: Int
+    varVariabel = 56 //assigning first value to varVariabel
+    println("Pemberian nilai pertama pada varVariabel = "+varVariabel)
+    varVariabel = 78 //assigning second value to varVariabel
+    println("Pemberian nilai kedua pada varVariabel = "+varVariabel)
+    var phi = 3.14
+    phi = 2.5
+}
