ceef1355458ca19b6a8f6fb3d75be68b397ae2c7
MithaZalinaRamadhany <mitazalina974@gmail.com>
2024-09-09 14:02:34 +0700

    Add Tugas.

--- src/main/kotlin/Boolean.kt
+++ src/main/kotlin/Boolean.kt
@@ -2,4 +2,7 @@ fun main(args: Array<String>) {
     var varBool : Boolean = true
     if(varBool)
         println("varBool bernilai " + varBool)
+    var varInt : Int = 1
+    if(varInt)
+        println("varInt bernilai " + varInt)
 }
